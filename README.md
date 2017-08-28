ce5387960421261adc8c4756c469aa8adf6d7986
yany <yanzhikai_yjk@qq.com>
2017-08-28 14:45:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+##介绍
+　　这是一个完全参考http://www.jianshu.com/p/b85812b6f7c1 博客写的。
+　　因为博客里面介绍的已经非常详细了，我只是按着指示操作来做的，没有技术含量。。。
+　　后续进行二次开发可以按照博客上写的继续做，不懂可以找我一齐探讨下。
+![](http://i.imgur.com/p1EFZxL.gif)
+
+##联系信息
+E-mail:yanzhikai_yjk@qq.com
---
+++ app/src/main/java/yanzhikai/qrcodedemo/MainActivity.java
@@ -0,0 +1,61 @@
+package yanzhikai.qrcodedemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import static android.R.attr.data;
+
+public class MainActivity extends AppCompatActivity {
+    private Button start_QR;
+    private TextView tv_show;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv_show = (TextView) findViewById(R.id.tv_show);
+        start_QR = (Button) findViewById(R.id.btn_startQR);
+        start_QR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建IntentIntegrator对象
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                //设置自定义扫描Activity
+                intentIntegrator.setCaptureActivity(CustomCaptureActivity.class);
+                //设置扫描提示
+                intentIntegrator.setPrompt("请将二维码对准扫描窗");
+                //设置扫描限时
+                intentIntegrator.setTimeout(30000);
+                // 开始扫描
+                intentIntegrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 获取解析结果
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "扫描取消", Toast.LENGTH_LONG).show();
+            } else {
+//                Toast.makeText(this, "扫描内容:" + result.getContents(), Toast.LENGTH_LONG).show();
+                tv_show.setText("扫描内容：\n" + result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
